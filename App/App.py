5e544ee3aa8c1fdbd627bf167fb15e70d4420678
Arpith25 <arpithworkv25@gmail.com>
2024-08-26 22:15:39 -0700

    Uploading files to the App folder

---
+++ App/App.py
@@ -0,0 +1,38 @@
+#Importing the required packages
+import sys
+
+sys.path.insert(0, 'C:/Users/Admin/Desktop/Project/Main')
+
+import Main
+
+
+#Calling Picke Files
+Main.intro()
+
+# Main.Naive_Bayes1()
+
+# Main.Naive_Bayes2()
+
+# Main.KNN1()
+
+# Main.KNN2()
+
+# Main.SVM1()
+
+# Main.SVM2()
+
+# Main.Decision_Tree1()
+
+# Main.Decision_Tree2()
+
+# Main.Decision_Tree_Hyp1()
+
+# Main.Decision_Tree_Hyp2()
+
+# Main.Random_Forest1()
+
+# Main.Random_Forest2()
+
+Main.Random_Forest_Hyp1()
+
+# Main.Random_Forest_Hyp2()
