237a3a94ccaea6cd9160f157dca096affb8b2d07
Arpith25 <arpithworkv25@gmail.com>
2024-08-26 22:20:43 -0700

    Uploading files to the Preprocessing folder

---
+++ Preprocessing/Preprocessing.py
@@ -0,0 +1,35 @@
+#Importing the required packages
+import sys
+
+sys.path.insert(0, 'C:/Users/Admin/Desktop/Project/Processing')
+
+import Processing
+
+
+#Creating object of the required modules
+p=Processing.EDA
+
+
+#Performing EDA on the dataset
+def processing_call():
+    
+    p.intro()
+    
+    X,Y,col=p.data_input()
+    
+    X,col=p.filling_values(X,col)
+    
+    X,col=p.converting_columns(X,col)
+    
+    X,col=p.outlier_detection(X,col)
+    
+    X,col=p.correlation_analysis(X,col)
+    
+    X21,Y21,X22,Y22=p.data_balancing(X,Y)
+    
+    X21_train,X21_test,Y21_train,Y21_test,X22_train,X22_test,Y22_train,Y22_test=p.splitting_data(X21,Y21,X22,Y22)
+
+    return X21_train,X21_test,Y21_train,Y21_test,X22_train,X22_test,Y22_train,Y22_test
+# import sys
+
+# print(sys.path)
