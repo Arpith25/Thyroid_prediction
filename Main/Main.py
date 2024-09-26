4edf96c4c9dc591dbe0174c38f7d0ce667bad10d
Arpith25 <arpithworkv25@gmail.com>
2024-09-25 23:03:28 -0700

    Update Main.py by adding the url for RFH1 as the path for RFH1

--- Main/Main.py
+++ Main/Main.py
@@ -728,7 +728,7 @@ class model_call:
     #Accessing the Random Forest algorithm with oversampling after hyperparameter tuning's pickle files
     def Random_Forest_Hyp1():
         
-        filepath='C:/Users/Admin/Desktop/Project/Model/RFHyperparameter1.pkl'
+        filepath='https://github.com/Arpith25/Thyroid_prediction/blob/main/Model/RFHyperparameter1.pkl'
 
         with open(filepath,'rb') as file13:
 
