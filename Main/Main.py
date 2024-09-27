8dd8457a701be5e875bff4f57340885bbc4b8522
Arpith25 <arpithworkv25@gmail.com>
2024-09-26 22:30:54 -0700

    Update Main.py by removing the specific error from the try-except blocks

--- Main/Main.py
+++ Main/Main.py
@@ -768,7 +768,7 @@ class model_call:
             
             a=float(input("Enter TSH:"))
 
-        except EOFError:      
+        except:      
             
             a = 1.2
 
@@ -776,7 +776,7 @@ class model_call:
     
             b=float(input("Enter T3:"))
 
-        except EOFError:
+        except:
 
             b = 15
 
@@ -784,7 +784,7 @@ class model_call:
     
             c=float(input("Enter TT4:"))
 
-        except EOFError:
+        except:
 
             c = 82
 
@@ -792,7 +792,7 @@ class model_call:
     
             d=float(input("Enter FTI:"))
 
-        except EOFError:
+        except:
 
             d = 80
 
