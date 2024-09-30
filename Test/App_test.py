57ead549f555e2296b5cf117ec5e974c1d650bd3
Arpith25 <arpithworkv25@gmail.com>
2024-09-29 23:00:50 -0700

    Update App_test.py by using mock tests

--- Test/App_test.py
+++ Test/App_test.py
@@ -1,4 +1,6 @@
 #Importing the required packages
+import unittest
+from unittest.mock import patch
 import sys
 
 sys.path.insert(0, 'Main')
@@ -35,6 +37,8 @@ class test_App:
 
     # Main.Random_Forest2()
 
-    model_call.Random_Forest_Hyp1()
+    def test_Random_Frest_Hyp1():
+        with patch('builtins.input', side_effect=[1.5,1.2,80,82])
+        #result=model_call.Random_Forest_Hyp1()
 
     # Main.Random_Forest_Hyp2()
