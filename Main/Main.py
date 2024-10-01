01a60a854d323b7c8e25ee0917392b2a714c933b
Arpith25 <arpithworkv25@gmail.com>
2024-10-01 02:55:04 -0700

    Update Main.py by removing the AssertionError

--- Main/Main.py
+++ Main/Main.py
@@ -795,7 +795,7 @@ def Random_Forest_Hyp1():
         print("Features:",features)
         
         print("Prediction:",pred3)
-    except (UnboundLocalError, AssertionError):
+    except UnboundLocalError:
         print("No values given")
 
 
