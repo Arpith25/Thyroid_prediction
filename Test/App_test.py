8583996023b24366c016bb2306e3d1083a1e3ffd
Arpith25 <arpithworkv25@gmail.com>
2024-09-30 20:13:15 -0700

    Update App_test.py by passing values to the mock_obj object

--- Test/App_test.py
+++ Test/App_test.py
@@ -38,9 +38,9 @@ class test_App:
     # Main.Random_Forest2()
 
     def test_Random_Forest_Hyp1():
-        mock_obj = Mock()
+        mock_obj = Mock(1.5, 12, 80, 82)
         Main.Random_Forest_Hyp1(mock_obj)
-        mock_obj.assert_called_with(1.5, 12, 80, 82)
+        mock_obj.assert_called_with()
         #result=model_call.Random_Forest_Hyp1()
 
     # Main.Random_Forest_Hyp2()
