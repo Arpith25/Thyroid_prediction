7b018e0c35b0f2c8ecba75114b46eb243046a389
Arpith25 <arpithworkv25@gmail.com>
2024-10-03 04:05:04 -0700

    Update App_test.py by moving the parametrize functionality outside the test_App class

--- Test/App_test.py
+++ Test/App_test.py
@@ -7,7 +7,7 @@ sys.path.insert(0, 'Main')
 
 import Main
 
-
+@pytest.mark.parametrize("a,b,c,d,o",[('15,1.2,80,82','P')])
 class test_App:
     
     #Calling Picke Files
@@ -37,7 +37,6 @@ class test_App:
 
     # Main.Random_Forest2()
 
-    @pytest.mark.parametrize("a,b,c,d,o",[('15,1.2,80,82','P')])
     def test_function(self,a,b,c,d,o):
         assert Main.Random_Forest_Hyp1(a,b,c,d) == o
 
